41b22563704c1c4fbfb4eb72472c658bdc9b0949
Jagdeep Singh <jagdeepkh@gmail.com>
2021-06-07 00:29:26 -0400

    Changes in DB connection

--- db.go
+++ db.go
@@ -10,14 +10,21 @@ import (
 
 // newDBClient ...
 func (config *Config) newDBClient() *pgxpool.Pool {
-	// Example dbUrl
+	// NOTE: Configure database
+	// https://pkg.go.dev/github.com/jackc/pgx/v4@v4.11.0/pgxpool#ParseConfig
+
+	// Example URL
 	// "postgres://username:password@localhost:5432/database_name"
 
-	var dbUrl string
+	// Example DSN
+	// "user=jack password=secret host=pg.example.com port=5432 dbname=mydb sslmode=verify-ca pool_max_conns=10"
+
+	var connString string
+
 	if os.Getenv("DATABASE_URL") != "" {
-		dbUrl = os.Getenv("DATABASE_URL")
+		connString = os.Getenv("DATABASE_URL")
 	} else {
-		dbUrl = fmt.Sprintf("postgres://%v:%v@%v:%v/%v?sslmode=disable",
+		connString = fmt.Sprintf("user=%v password=%v host=%v port=%v dbname=%v sslmode=none pool_max_conns=5",
 			config.Database.Username,
 			config.Database.Password,
 			config.Database.Host,
@@ -26,7 +33,20 @@ func (config *Config) newDBClient() *pgxpool.Pool {
 		)
 	}
 
-	dbpool, err := pgxpool.Connect(context.Background(), dbUrl)
+	// NOTE: Using github.com/jackc/pgx/v4
+	// https://pkg.go.dev/github.com/jackc/pgx/v4@v4.11.0/pgxpool#pkg-overview
+
+	dbConfig, err := pgxpool.ParseConfig(connString)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to parse database config: %v\n", err)
+		os.Exit(1)
+	}
+
+	// dbConfig.AfterConnect = func(ctx context.Context, conn *pgx.Conn) error {
+	// 	// do something with every new connection
+	// }
+
+	dbpool, err := pgxpool.ConnectConfig(context.Background(), dbConfig)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
 		os.Exit(1)
