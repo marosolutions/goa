48b586e22c40a2500c98386fcc55b56be127fff4
Jagdeep Singh <jagdeepkh@gmail.com>
2021-06-07 02:09:30 -0400

    added StructToKeysAndValues

--- helpers.go
+++ helpers.go
@@ -1,6 +1,9 @@
 package goa
 
-import "strings"
+import (
+	"reflect"
+	"strings"
+)
 
 // contains checks if a string is present in a slice
 func SliceContains(s []string, str string) bool {
@@ -23,4 +26,17 @@ func DeleteEmpty(s []string) []string {
 		}
 	}
 	return r
+}
+
+// StructToKeysAndValues: Convert a struct to Keys and Values mainly for SQL Inserts
+func StructToKeysAndValues(tag string, s interface{}) (keys []string, vals []interface{}) {
+	v := reflect.ValueOf(s)
+	typeOfS := v.Type()
+
+	for i := 0; i < v.NumField(); i++ {
+		// cols = append(cols, typeOfS.Field(i).Name)
+		keys = append(keys, typeOfS.Field(i).Tag.Get(tag))
+		vals = append(vals, v.Field(i).Interface())
+	}
+	return
 }
